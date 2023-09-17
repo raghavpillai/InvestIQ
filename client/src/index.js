84fecda216b08a1457c193dc631424a195d4e880
Nikhil Narvekar <nikhil.narvekar.712@gmail.com>
2023-09-17 06:17:56 -0500

    basic layout done

--- client/src/index.js
+++ client/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { createBrowserRouter,  RouterProvider } from "react-router-dom"
-
+import Stock from './Stock';
 
 const router = createBrowserRouter([
   {
@@ -12,8 +12,8 @@ const router = createBrowserRouter([
     element: <App/>
   },
   {
-    path: "/easteregg",
-    element: <div>Hello World</div>
+    path: "/stock/:company",
+    element: <Stock/>
   }
 ])
 
